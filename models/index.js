d44eb9fb650eb2e3163e672e690c876495dea1b3
sobitd <sobitdaniel@gmail.com>
2023-03-06 08:12:53 +0300

    modify index.js

--- models/index.js
+++ models/index.js
@@ -20,5 +20,21 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.articles = require("./article.model.js")(sequelize, Sequelize);
+db.user = require("../models/user.model")(sequelize, Sequelize);
+db.role = require("../models/role.model")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+
+db.user.belongToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+
+db.ROLES = ["user", "author", "editor", "supervisor", "principal", "admin"];
 
 module.exports = db;

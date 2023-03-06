9a935d23cc61bc6576e5bff223103fec3da897d1
sobitd <sobitdaniel@gmail.com>
2023-03-06 22:59:57 +0300

    Add file.controller.js

---
+++ controllers/file.controller.js
@@ -0,0 +1,21 @@
+const uploadFile = require("../middleware/upload");
+const upload = async (req, res) => {
+  try {
+    await uploadFile(req, res);
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Upload a file please!" });
+    }
+    res.status(200).send({
+      message:
+        "The following file was uploaded successfully: " +
+        req.file.originalname,
+    });
+  } catch (err) {
+    // error handling
+    res.status(500).send({
+      message: `Unable to upload the file: ${req.file.originalname}. ${err}`,
+    });
+  }
+};
+
+module.exports = upload;
